b2426f9436f37826a185e1aac705d225753855e4
SSKnT <sohaibshaukat78@gmail.com>
2025-04-10 08:38:29 +0500

    add Button component with customizable text

---
+++ src/components/button.js
@@ -0,0 +1,6 @@
+
+const Button = ({text}) => {
+    return(
+        <button className="px-4 py-2">{text}</button>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,6 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div className="">
-s
     </div>
   );
 }
