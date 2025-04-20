fa5e206cf9f8c0d220907576f7dc02e6c3a92805
SSKnT <sohaibshaukat78@gmail.com>
2025-04-20 23:34:00 +0500

    feat: implement dynamic imports for PrivacyChallenge and CryptoChallenge components with loading skeletons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,28 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Bebas_Neue } from 'next/font/google';
+import { FlagProvider } from "@/hooks/flagContext";
+import dynamic from 'next/dynamic'
+import Skeleton from "react-loading-skeleton";
+
+import CaptureTheFlag from "@/components/Sections/welcomeSection";
+import HowToPlay from "@/components/Sections/howToPlay";
+import FlagCounter from "@/components/flagsCounter";
+
+const LazyPrivacyChallenge = dynamic(() => import('@/components/Sections/privacyPolicy'), {
+  loading: () => <Skeleton count={1} width={100} height={60} />,
+  ssr: false,
+});
+
+const LazyCryptoChallenge = dynamic(() => import('@/components/Sections/cryptoChallenge'), {
+  loading: () => <Skeleton count={1} width={100} height={60} />,
+  ssr: false,
+});
 
 const bebas = Bebas_Neue({
   subsets: ['latin'],
   weight: '400',
   variable: '--font-bebas',
 });
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-import CaptureTheFlag from "@/components/Sections/welcomeSection";
-import HowToPlay from "@/components/Sections/howToPlay";
-import PrivacyChallenge from "@/components/Sections/privacyPolicy";
-import FlagCounter from "@/components/flagsCounter";
-import CryptoChallenge from "@/components/Sections/cryptoChallenge";
-
-import { FlagProvider } from "@/hooks/flagContext";
 
 
 export default function Home() {
@@ -34,8 +33,8 @@ export default function Home() {
           <FlagCounter count={1} />
           <CaptureTheFlag />
           <HowToPlay />
-          <PrivacyChallenge />
-          <CryptoChallenge />
+          <LazyPrivacyChallenge />
+          <LazyCryptoChallenge />
         </main>
       </div>
     </FlagProvider>
